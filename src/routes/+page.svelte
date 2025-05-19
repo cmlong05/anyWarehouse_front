c93ad5cb4ca0ec9c3e1f53f7672cfd63e6f7ba39
cmlong-MECHREVO <37834464+cmlong05@users.noreply.github.com>
2025-05-19 13:21:33 +0800

    pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<h1>home</h1>
+<p>this is the home page.</p>
