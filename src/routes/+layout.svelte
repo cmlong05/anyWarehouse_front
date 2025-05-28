d58980e8542d243db2455886ef4d344e0fb103f0
menglong <chen.menglong@outlook.com>
2025-05-28 18:03:49 +0800

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	<a href="/">home</a>
 	<a href="/container/Office" >Container</a>
 	<a href="/items">Items</a>
-	<a href="/categories">Categories</a>
+	<a href="/categories/4">Categories</a>
 </nav> 
 
 {@render children()}
