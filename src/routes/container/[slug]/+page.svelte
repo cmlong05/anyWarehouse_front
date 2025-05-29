64875fabd511bc2ef930ec740437d80eddc1afe6
cmlong05 <cmlong05@gmail.com>
2025-05-29 20:05:47 +0800

    modify

--- src/routes/container/[slug]/+page.svelte
+++ src/routes/container/[slug]/+page.svelte
@@ -18,8 +18,29 @@
 <div class="main-content">
 
     <h2>本容器</h2>
-    <p><strong>编号：</strong>{data.containerRes.container.fastCode}</p>
+    <p><strong>编号：{data.containerRes.container.fastCode}</strong></p>
     <p>{data.containerRes.container.mark}</p>
+    <p>条形码：{data.containerRes.container.barcode}</p>
+    <div style="display: flex; align-items: center; gap: 1em; margin: 1em 0;">
+        <span>
+            容量剩余：
+            <strong>{data.containerRes.container.a_volume}</strong>
+            /
+            <strong>{data.containerRes.container.volume}</strong>
+        </span>
+        <meter
+            value={data.containerRes.container.a_volume}
+            min="0"
+            max={data.containerRes.container.volume}
+            style="
+                display: block;
+                width: {Math.max(100, data.containerRes.container.volume / 100)}px; 
+                <!-- 需要调整 -->
+                height: {Math.max(1.5, 66666/ 10000)}em;"
+        ></meter>
+    </div>
+
+
 
     <h2>子容器</h2>
     {#each data.containerRes.descendants as descendant}
@@ -34,15 +55,18 @@
     {/each}
 
     <h2>存储信息</h2>
-    {#each data.containerRes.storages as storage}
-        <div>
-            <p>{storage.quantity} *
-                <a href={`/items/${storage.item_id}`}>{storage.item_SKU}
-                </a> {storage.item_name}
-            </p>
-            
-        </div>
-    {/each}
+    {#if data.containerRes.storages.length === 0}
+        <p>暂无存储信息。</p>
+    {:else}
+        {#each data.containerRes.storages as storage}
+            <div>
+                <p>{storage.quantity} *
+                    <a href={`/items/${storage.item_id}`}>{storage.item_SKU}
+                    </a> {storage.item_name}
+                </p>
+            </div>
+        {/each}
+    {/if}
 </div>
 
 <!-- 侧栏 -->
