6fb0e7d377ff4ae740a49a4114470f888efc8924
cmlong05 <cmlong05@gmail.com>
2025-06-05 22:06:19 +0800

    update item layout

--- src/routes/item/[slug]/+page.svelte
+++ src/routes/item/[slug]/+page.svelte
@@ -6,38 +6,37 @@
 
 <!-- 导航格 -->
 <div class="navigation">
-
+    <span>导航：</span>
+    {#each data.itemDetail.categories as category}
+        {#each category.ancestors as ancestor, i}
+            <a href="/container/{ancestor.id}">{ancestor.name}</a>
+            {#if i < category.ancestors.length}
+                <span> &gt;&thinsp; </span>
+            {/if}
+        {/each}
+    {/each}    
+    <span></span>
 </div>
 
 <!-- 主内容 -->
 <div class="main-content">
-        <div class="item-details">
-        <div>
-            <p>{data.itemDetail.item.name}</p>
-        </div>
-        <div>
-            <div class="item-container">
-                <div class="image-container">
-                    <img 
-                        src={data.itemDetail.item.image ? `${API_BASE_URL}${data.itemDetail.item.image.trim()}` : ''} 
-                        style="max-width: 100%; height: auto; max-height: 400px;" 
-                        alt={data.itemDetail.item.name} 
-                        loading="lazy" 
-                    />
-                </div>
-                <div class="info-container">
-                    <p><strong>SKU:</strong> {data.itemDetail.item.SKU}</p>
-                    <p class = "text-p">{data.itemDetail.item.description || '无描述'}</p>
-                    <p><strong>重量:</strong> {data.itemDetail.item.weight} 克</p>
-                    <p><strong>体积 (P):</strong> {data.itemDetail.item.p_volume} 立方厘米</p>
-                    <p><strong>体积 (S):</strong> {data.itemDetail.item.s_volume} 立方厘米</p>
-                    <p><strong>价格:</strong> {data.itemDetail.item.b_Price} {data.itemDetail.item.currency || '货币单位未知'}</p>
-                    <p><strong>添加日期:</strong> {data.itemDetail.item.date_added}</p>
-                    <p><strong>分类:</strong> {data.itemDetail.item.category.join(', ')}</p>
-                    <p><strong>组件:</strong> {data.itemDetail.item.components.length > 0 ? data.itemDetail.item.components.join(', ') : '无组件'}</p>
-                </div>
-            </div>
-        </div>
+    <div class="image-container">
+        <img 
+            src={data.itemDetail.item.image ? `${API_BASE_URL}${data.itemDetail.item.image.trim()}` : ''} 
+            style="max-width: 100%; height: auto; max-height: 400px;" 
+            alt={data.itemDetail.item.name} 
+            loading="lazy" 
+        />
+    </div>
+    <div class="info-container">
+        <p>{data.itemDetail.item.name}</p>
+        <p><strong>SKU:</strong> {data.itemDetail.item.SKU}</p>
+        <p class = "text-p">{data.itemDetail.item.description || '无描述'}</p>
+        <p><strong>重量:</strong> {data.itemDetail.item.weight} 克</p>
+        <p><strong>体积 (S):</strong> {data.itemDetail.item.s_volume} 立方厘米</p>
+        <p><strong>价格:</strong> {data.itemDetail.item.b_Price} {data.itemDetail.item.currency || '货币单位未知'}</p>
+        <p><strong>分类:</strong> {data.itemDetail.item.category.join(', ')}</p>
+        <p><strong>组件:</strong> {data.itemDetail.item.components.length > 0 ? data.itemDetail.item.components.join(', ') : '无组件'}</p>
     </div>
 </div>
 
