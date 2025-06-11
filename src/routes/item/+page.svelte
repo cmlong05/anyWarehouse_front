6dbad7ed89b3ffa42a3beb382ad8ceae3850ae91
cmlong-MECHREVO <37834464+cmlong05@users.noreply.github.com>
2025-06-11 23:47:20 +0800

    improve

--- src/routes/item/+page.svelte
+++ src/routes/item/+page.svelte
@@ -3,15 +3,12 @@
     let { data } = $props<{ items: Items[] }>();
 </script>
 
-<h1>Items</h1>
 {#if data.items.length > 0}
     <ul>
         {#each data.items as { id, SKU, name }}
             <li><a href={`/item/${id}`}>{SKU}</a> - {name}</li>
         {/each}
     </ul>
-{:else}
-    <p>正在加载物品列表...</p>
 {/if}
-<p>this is the Items page.</p>
+
 <mcfile name="+page.svelte" path="e:\anyWarehouse_front\src\routes\items\+page.svelte"></mcfile>
