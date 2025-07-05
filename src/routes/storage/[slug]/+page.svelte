3dc66b936332753fa4841f030c2837ebc4b87c99
cmlong05 <cmlong05@gmail.com>
2025-07-06 01:10:15 +0800

    small

--- src/routes/storage/[slug]/+page.svelte
+++ src/routes/storage/[slug]/+page.svelte
@@ -8,9 +8,35 @@
 </svelte:head>
 
 <main>
-    <!-- 直接以foam的形式展示，设置开关按钮，开启的关闭编辑模式 -->
-    <h1>{data.storageDetail.id}</h1>
-    <p>{data.storageDetail.item}</p>
-    <p>{data.storageDetail.container}</p>
-    <p>{data.storageDetail.quantity}</p>
+    <!-- 直接以form的形式展示 -->
+    <form>
+        <span>id: {data.storageDetail.id}</span>
+        <label>
+            名称
+            <input type="text" value={data.storageDetail.item} />
+        </label>
+        <label>
+            位置
+            <input type="text" value={data.storageDetail.container} />
+        </label>
+        <label>
+            数量
+            <input type="text" value={data.storageDetail.quantity} />
+        </label>
+        <label>
+            备注
+            <input type="text" value={data.storageDetail.text} />
+        </label>
+        <label>
+            样品
+            <input type="checkbox" value={data.storageDetail.sample} />
+        </label>
+
+        <div style="display: flex; gap: 10px;">
+            <button>提交修改</button>
+            <button>取消修改</button>
+            <button>删除</button>
+        </div>
+    </form>
+
 </main>
