6b12b4db6df4c0c348278b93f55815803151bd1e
cmlong05 <cmlong05@gmail.com>
2025-07-10 00:12:56 +0800

    add tailwindcss

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import type { Config } from '@sveltejs/kit';
 
@@ -11,9 +11,7 @@ const config: Config = {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter({
-            fallback: 'index.html' // 启用 SPA 回退
-        })
+        adapter: adapter()
     }
 };
 
